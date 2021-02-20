175a2136474d623b7a268f182b617ad44139f5c0
milad <miladxgh@outlook.com>
2021-02-20 15:31:14 +0330

    btn animation added

--- app.js
+++ app.js
@@ -14,11 +14,12 @@ const alph = [
 ];
 
 const beep = new Audio("snd/beep.wav");
-let active = false;
+var active = false;
 
 // DOM
 let morseBtn = document.getElementById("morse-btn");
 let clearBtn = document.getElementById("clear-btn");
+let deleteBtn = document.getElementById("delete-btn");
 let morseScreen = document.getElementById("morse-screen");
 let textScreen = document.getElementById("text-screen");
 
@@ -45,20 +46,26 @@ function clrTextScreen() {
 let flag1, flag2;
 // morse button mousedown event
 morseBtn.addEventListener("mousedown", function() {
+    beep.play();
     flag1 = new Date().getTime();
     
-    beep.currentTime = 1;
-    beep.play();
-
-    if(active) clearTimeout(mt);
-    clearTimeout(st);
+    beep.currentTime = 0.1;
+    
+    if(active) {
+        clearTimeout(mt);
+        clearTimeout(st);
+    } 
+    // style
+    this.style.width = "95px";
+    this.style.height = "95px";
 });
 
 // morse button mouseup even
 morseBtn.addEventListener("mouseup", function() {
-
+    beep.pause();
     flag2 = new Date().getTime();
     let passed = flag2 - flag1;
+
     if (passed < 150) {
         setMorseScreen(".");
     } else {
@@ -66,7 +73,7 @@ morseBtn.addEventListener("mouseup", function() {
     }
 
     mt = setTimeout(function() {
-        active = true;
+        
         let index = morseAlph.indexOf(getMorseScreen());
         if (morseAlph.includes(getMorseScreen())) {
             setTextScreen(alph[index]);
@@ -79,10 +86,38 @@ morseBtn.addEventListener("mouseup", function() {
         setTextScreen("\u00A0"); // blank space
     }, 1000);
 
-    beep.pause();
+    active = true;
+    // style
+
+    this.style.width = "100px";
+    this.style.height = "100px";
 });
 
 
-clearBtn.addEventListener("click", function() {
+clearBtn.addEventListener("mousedown", function() {
     textScreen.innerText = '';
+    // style
+    this.style.width = "45px";
+    this.style.height = "45px";
 });
+
+clearBtn.addEventListener("mouseup", function() {
+    // style
+    this.style.width = "50px";
+    this.style.height = "50px";
+});
+
+
+deleteBtn.addEventListener("mousedown", function() {
+    textScreen.innerText = '';
+    // style
+    this.style.width = "45px";
+    this.style.height = "45px";
+});
+
+deleteBtn.addEventListener("mouseup", function() {
+    // style
+    this.style.width = "50px";
+    this.style.height = "50px";
+});
+
