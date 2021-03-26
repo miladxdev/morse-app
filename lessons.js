54681105a2b0b0b4058cd36371ccf322a5938c51
milad <miladxgh@outlook.com>
2021-03-26 19:21:59 +0430

    check mark if answer is correct

--- lessons.js
+++ lessons.js
@@ -1,17 +1,23 @@
 let tryElement = document.getElementById("try");
 let scoreElement = document.getElementById("score");
 
-let randomAlph = alph[Math.floor(Math.random() * alph.length)];
+let randomAlph = alph[Math.floor(Math.random() * 26)];
 let score = 0;
 
-
 tryElement.innerHTML = "Try: " + randomAlph;
+        
 
 function checkAnswer() {
     if (getTextScreen() == randomAlph) {
         score += 5;
         scoreElement.innerHTML = 'score: ' + score;
-        randomAlph = alph[Math.floor(Math.random() * alph.length)];
-        tryElement.innerHTML = "Try: " + randomAlph;
+        randomAlph = alph[Math.floor(Math.random() * 26)];
+        tryElement.innerHTML += "&nbsp"+"<img src='icon/check-icon.png' style='width:20px; position:absolute; filter:none'>";
+        scoreElement.style.color = "lime";
+        setTimeout(function() {
+            tryElement.innerHTML = "Try: " + randomAlph;
+            scoreElement.style.color = "#c8c8c8";
+            clrTextScreen();
+        }, 2000);
     }
 }
