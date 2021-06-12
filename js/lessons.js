9b67518ac99737c0f8d11cea77225b3abf1f55c1
Milad <miladxgh@outlook.com>
2021-06-13 00:38:03 +0430

    score system and progress bar

--- js/lessons.js
+++ js/lessons.js
@@ -1,22 +1,38 @@
 let tryElement = document.getElementById("try");
 let scoreElement = document.getElementById("score");
+let xpElem = document.querySelector(".xp");
 
-let randomAlph = alph[Math.floor(Math.random() * 26)];
 let score = 0;
+let randomWord = "";
+let words = 1;
+let temp = "";
 
-tryElement.innerHTML = "Try: " + randomAlph;
+for (let i = 0; i < words; i++) {
+  randomWord += alph[Math.floor(Math.random() * 26)];
+}
+tryElement.innerHTML = randomWord;
 
 function checkAnswer() {
-    if (getTextScreen() == randomAlph) {
-        score += 5;
-        scoreElement.innerHTML = 'score: ' + score;
-        randomAlph = alph[Math.floor(Math.random() * 26)];
-        tryElement.innerHTML += "&nbsp"+"<img src='icon/check-icon.png' style='width:20px; position:absolute; filter:none'>";
-        scoreElement.style.color = "lime";
-        setTimeout(function() {
-            tryElement.innerHTML = "Try: " + randomAlph;
-            scoreElement.style.color = "#c8c8c8";
-            clrTextScreen();
-        }, 2000);
+  if (getTextScreen() == randomWord) {
+    score += 5;
+    words = Math.floor(score / 100) + 1;
+    // console.log(words);
+    randomWord = "";
+    scoreElement.innerHTML = score;
+    for (let i = 0; i < words; i++) {
+      randomWord += alph[Math.floor(Math.random() * 26)];
     }
+
+    // tryElement.innerHTML += "&nbsp" + "<img src='icon/check-icon.png' style='width:20px; position:absolute; filter:none'>";
+    tryElement.style.color = "lime";
+    scoreElement.style.color = "lime";
+    xpElem.style.width = score + "%";
+
+    setTimeout(function () {
+      tryElement.innerHTML = randomWord;
+      scoreElement.style.color = "#c8c8c8";
+      tryElement.style.color = "#c8c8c8";
+      clrTextScreen();
+    }, 2000);
+  }
 }
