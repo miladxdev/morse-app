35898c203827bf0f9380ea4768941eac3146887e
Milad <miladxgh@outlook.com>
2021-06-16 20:31:38 +0430

    ocean theme added

--- app.js
+++ app.js
@@ -1,17 +1,101 @@
-
 const morseAlph = [
-    ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",
-    ".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",
-    ".--","-..-","-.--","--..","-----",".----","..---","...--","....-",
-    ".....","-....","--...","---..","----.",".-.-.-","--..--","---...",
-    "..--..",".----.","-....-","-..-.",".-..-.",".--.-.","-...-","---."
+  ".-",
+  "-...",
+  "-.-.",
+  "-..",
+  ".",
+  "..-.",
+  "--.",
+  "....",
+  "..",
+  ".---",
+  "-.-",
+  ".-..",
+  "--",
+  "-.",
+  "---",
+  ".--.",
+  "--.-",
+  ".-.",
+  "...",
+  "-",
+  "..-",
+  "...-",
+  ".--",
+  "-..-",
+  "-.--",
+  "--..",
+  "-----",
+  ".----",
+  "..---",
+  "...--",
+  "....-",
+  ".....",
+  "-....",
+  "--...",
+  "---..",
+  "----.",
+  ".-.-.-",
+  "--..--",
+  "---...",
+  "..--..",
+  ".----.",
+  "-....-",
+  "-..-.",
+  ".-..-.",
+  ".--.-.",
+  "-...-",
+  "---.",
 ];
 
 const alph = [
-    "A","B","C","D","E","F","G","H","I","J","K","L","M",
-    "N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-    "0","1","2","3","4","5","6","7","8","9",
-    ".",",",":","?","'","-","/",'"',"@","=","!"
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  ".",
+  ",",
+  ":",
+  "?",
+  "'",
+  "-",
+  "/",
+  '"',
+  "@",
+  "=",
+  "!",
 ];
 
 const beep = new Audio("snd/beep.wav");
@@ -31,105 +115,99 @@ let muteCheckbox = document.querySelector("#mute");
 // functions
 getMorseScreen = () => morseScreen.innerText;
 
-setMorseScreen = (m) => morseScreen.innerText += m;
+setMorseScreen = (m) => (morseScreen.innerText += m);
 
-clrMorseScreen = () => morseScreen.innerText = '';
+clrMorseScreen = () => (morseScreen.innerText = "");
 
 getTextScreen = () => text.innerText;
 
-setTextScreen = (t) => text.innerHTML += t;    
+setTextScreen = (t) => (text.innerHTML += t);
 
-clrTextScreen = () => text.innerText = '';
+clrTextScreen = () => (text.innerText = "");
 
-let flag1, flag2;
-
+let flag1, flag2, mt, st;
 // ...morse button mousedown event
 morseBtn.addEventListener("mousedown", () => {
-    beep.play();
-    beep.volume = muteCheckbox.checked ? 0 : 1;
-    flag1 = new Date().getTime();
-    if(isMoueUp) {
-        clearTimeout(mt);
-        clearTimeout(st);
-    } 
+  beep.play();
+  beep.volume = muteCheckbox.checked ? 0 : 1;
+  flag1 = new Date().getTime();
+  clearTimeout(mt);
+  clearTimeout(st);
 });
 
 // ..morse button mouseup even
 morseBtn.addEventListener("mouseup", () => {
-    beep.pause();
-    beep.currentTime = 0.1;
-    flag2 = new Date().getTime();
-    let passed = flag2 - flag1;
-    if (passed < ping) {
-        setMorseScreen(".");
-    } else if (passed < 3*ping) {
-        setMorseScreen("-");
+  beep.pause();
+  beep.currentTime = 0.1;
+  flag2 = new Date().getTime();
+  let passed = flag2 - flag1;
+  if (passed < ping) {
+    setMorseScreen(".");
+  } else if (passed < 3 * ping) {
+    setMorseScreen("-");
+  }
+
+  mt = setTimeout(() => {
+    // translate morse
+    let index = morseAlph.indexOf(getMorseScreen());
+    if (morseAlph.includes(getMorseScreen())) {
+      setTextScreen(lowerCheckbox.checked ? alph[index].toLowerCase() : alph[index].toUpperCase());
+      st = setTimeout(() => {
+        setTextScreen("&nbsp;");
+      }, 7 * ping); // prin blank space
+      checkAnswer();
     }
 
-    mt = setTimeout(() => { // translate morse
-        let index = morseAlph.indexOf(getMorseScreen());
-        if (morseAlph.includes(getMorseScreen())) {
+    clrMorseScreen();
+  }, 3 * ping);
 
-            setTextScreen(lowerCheckbox.checked ? alph[index].toLowerCase() : alph[index].toUpperCase());
-            st = setTimeout(() => {setTextScreen('&nbsp;');}, 7*ping ); // prin blank space
-            checkAnswer();
-               
-        }
-
-        clrMorseScreen();
-    }, 3*ping);
-    
-    isMoueUp = true;
-    
+  isMoueUp = true;
 });
-
 
 // ..clear button events
 clearBtn.addEventListener("mousedown", () => {
-    text.innerText = '';
-    clearTimeout(st);
+  text.innerText = "";
+  clearTimeout(st);
 });
-
-
 
 // ..delete button events
 deleteBtn.addEventListener("mousedown", () => {
-    let text = getTextScreen();
-    text = text.slice(0, -1);
-    clrTextScreen();
-    setTextScreen(text);
-    clearTimeout(st);
+  let text = getTextScreen();
+  text = text.slice(0, -1);
+  clrTextScreen();
+  setTextScreen(text);
+  clearTimeout(st);
 });
 
 // show hide alphabet
-const settingsSec = document.querySelector('.settings');
-const settingBtn = document.querySelector('#set-btn');
-
+const settingsSec = document.querySelector(".settings");
+const settingBtn = document.querySelector("#set-btn");
 var toggle = true;
-settingBtn.addEventListener('click', () => {
-    if (settingsSec.style.height == '650px') {
-        settingsSec.style.height = '0';
-        settingsSec.style.opacity = '0';
-        toggle = !toggle;
-    } else {
-        settingsSec.style.height = '650px';
-        settingsSec.style.opacity = '1';
-        toggle = !toggle;
-    }
+
+settingBtn.addEventListener("click", () => {
+  if (settingsSec.style.height == "650px") {
+    settingsSec.style.height = "0";
+    settingsSec.style.opacity = "0";
+    toggle = !toggle;
+  } else {
+    settingsSec.style.height = "650px";
+    settingsSec.style.opacity = "1";
+    toggle = !toggle;
+  }
 });
 
-
 // toggle cheat code
-const cheatCodeSec = document.querySelector('.cheat-code');
-const cheatBtn = document.querySelector('#cheat-btn');
-cheatBtn.addEventListener('click', () => {
-    if (cheatCodeSec.style.height == '650px') {
-        cheatCodeSec.style.height = '0';
-        cheatCodeSec.style.opacity = '0';
-        toggle = !toggle;
-    } else {
-        cheatCodeSec.style.height = '650px';
-        cheatCodeSec.style.opacity = '1';
-        toggle = !toggle;
-    }
+const cheatCodeSec = document.querySelector(".cheat-code");
+const cheatBtn = document.querySelector("#cheat-btn");
+
+cheatBtn.addEventListener("click", () => {
+  if (cheatCodeSec.style.height == "650px") {
+    cheatCodeSec.style.height = "0";
+    cheatCodeSec.style.opacity = "0";
+    toggle = !toggle;
+  } else {
+    cheatCodeSec.style.height = "650px";
+    cheatCodeSec.style.opacity = "1";
+    toggle = !toggle;
+  }
 });
--- js/lessons.js
+++ js/lessons.js
@@ -1,38 +1,42 @@
 let tryElement = document.getElementById("try");
 let scoreElement = document.getElementById("score");
 let xpElem = document.querySelector(".xp");
+const trainingCheckbox = document.getElementById("training");
 
 let score = 0;
 let randomWord = "";
 let words = 1;
 let temp = "";
 
+console.log(trainingCheckbox.checked);
 for (let i = 0; i < words; i++) {
   randomWord += alph[Math.floor(Math.random() * 26)];
 }
 tryElement.innerHTML = randomWord;
 
 function checkAnswer() {
-  if (getTextScreen() == randomWord) {
-    score += 5;
-    words = Math.floor(score / 100) + 1;
-    // console.log(words);
-    randomWord = "";
-    scoreElement.innerHTML = score;
-    for (let i = 0; i < words; i++) {
-      randomWord += alph[Math.floor(Math.random() * 26)];
+  if (trainingCheckbox.checked) {
+    if (getTextScreen() == randomWord) {
+      score += 5;
+      words = Math.floor(score / 100) + 1;
+      // console.log(words);
+      randomWord = "";
+      scoreElement.innerHTML = score;
+
+      for (let i = 0; i < words; i++) {
+        randomWord += alph[Math.floor(Math.random() * 26)];
+      }
+
+      tryElement.style.color = "lime";
+      scoreElement.style.color = "lime";
+      xpElem.style.width = score + "%";
+
+      setTimeout(function () {
+        tryElement.innerHTML = randomWord;
+        scoreElement.style.color = "#c8c8c8";
+        tryElement.style.color = "#c8c8c8";
+        clrTextScreen();
+      }, 2000);
     }
-
-    // tryElement.innerHTML += "&nbsp" + "<img src='icon/check-icon.png' style='width:20px; position:absolute; filter:none'>";
-    tryElement.style.color = "lime";
-    scoreElement.style.color = "lime";
-    xpElem.style.width = score + "%";
-
-    setTimeout(function () {
-      tryElement.innerHTML = randomWord;
-      scoreElement.style.color = "#c8c8c8";
-      tryElement.style.color = "#c8c8c8";
-      clrTextScreen();
-    }, 2000);
   }
 }
